4230c8cf6a163395c5685dc0a089a7284c1416f5
unknown <pbiswajit856@gmail.com>
2022-03-27 15:51:33 +0530

    final commit

--- src/component/Success.js
+++ src/component/Success.js
@@ -1,9 +1,19 @@
 import React from 'react'
 
 export default function Success() {
-  return (
-    <div>
-      sucess
-    </div>
-  )
+    return (
+        <div>
+            <h1>Password Changed</h1>
+            <span>Your password has been updated! Now get back<br/>
+                in the driver seat, racers are waiting for you!</span>
+               <div><button style={{width: "250px",
+height: '40px',
+color: 'white',
+border: 'none',
+marginLeft: '30px',
+marginTop: '40px',
+background: '#000000',
+borderRadius: '10px'}}>JOIN A RACE</button></div> 
+        </div>
+    )
 }
